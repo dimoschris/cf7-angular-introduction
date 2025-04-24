e8f6d988c3e7d691ed735837024d0cec3572ca4b
Dimos Chris <d.christoforoy@gmail.com>
2025-04-24 20:43:39 +0300

    Initial commit

---
+++ src/app/components/person-table/person-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-table',
+  imports: [],
+  templateUrl: './person-table.component.html',
+  styleUrl: './person-table.component.css'
+})
+export class PersonTableComponent {
+
+}
